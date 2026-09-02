2dde19444851ec02a8e4e7bf410b69283171e6f1
YaroslavBezghin <b.yaroslav88@gmail.com>
2026-09-02 12:42:23 -0700

    Add: products-of-array-discluding-self - submission-14

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-14.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        prefix = [1]
+        for i in range(len(nums) - 1):
+            prefix.append(prefix[i] * nums[i])
+        postfix = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            postfix[i] = postfix[i + 1] * nums[i + 1]
+        result = []
+        for i in range(len(nums)):
+            result.append(prefix[i] * postfix[i])
+        return result
+
+# Time: O(n)
+# Space: O(n)
+        """
+        result = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] *= prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
+# Time: O(n)
+# Space: O(1), array retuned does not count
